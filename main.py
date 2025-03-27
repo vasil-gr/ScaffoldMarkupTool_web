54328d950b2935dee55903b4941e7272b948ef22
Vasilii <vasya.31.46@gmail.com>
2025-03-27 18:42:28 +0300

    Add main.py as future merge canvas and sidepanel
    Initial skeleton with: basic step navigation structure, placeholders for canvas/sidepanel integration, session state management

---
+++ main.py
@@ -0,0 +1,65 @@
+import streamlit as st
+
+
+# Функция для обработки кнопки "Next"
+def next_step():
+    if (st.session_state.step == 1 and st.session_state.original_img is None) or st.session_state.step == 3:
+        return
+    else:
+        st.session_state.step += 1
+
+# Функция для обработки кнопки "Back"
+def back_step():
+    if st.session_state.step == 2: # зачем нужно: пользователь загружает проект с точками, шаг 1 -> шаг 2 -> шаг 1 -> шаг 2 -> точки должны перерисовываться
+        st.session_state.step2_initial_render = True
+    if st.session_state.step > 1:
+        st.session_state.step -= 1
+
+# Функция для обработки кнопки "Restart"
+def restart():
+    st.session_state.step = 1
+    st.session_state.clear() # полная очистка всех состояний
+
+
+
+def main():
+
+    # css и пр
+    # ...
+
+    # инициализация переменных
+    # ...
+    
+    # Боковая панель
+    if st.session_state.sidebar_state == "expanded":
+                
+        with st.sidebar:
+            
+            # Контент для каждого шага
+            if st.session_state.step == 1:
+                pass
+            elif st.session_state.step == 2:
+                pass
+            elif st.session_state.step == 3:
+                pass
+            
+            # Общие кнопки навигации ("Back", "Next" и "Restart")
+            col1, col2, col3 = st.columns([1, 1, 1])
+            with col1:
+                st.button("Restart", on_click=restart, disabled=st.session_state.step == 1)
+            with col2:
+                st.button("Back", on_click=back_step, disabled=st.session_state.step == 1)
+            with col3:
+                st.button("Next", on_click=next_step, disabled=((st.session_state.step == 1 and st.session_state.original_img is None) or (st.session_state.step == 3)))
+
+    
+    # Основное окно для каждого шага
+    if st.session_state.step == 1:
+        pass
+    elif st.session_state.step == 2:
+        pass
+    elif st.session_state.step == 3:
+        pass
+
+if __name__ == "__main__":
+    main()
