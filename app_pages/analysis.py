933fd5ee156fa7727ebbd9e83f8b3dadaba44c53
Vasilii <vasya.31.46@gmail.com>
2025-04-25 23:55:49 +0300

    Created entry point analysis.py

--- app_pages/analysis.py
+++ app_pages/analysis.py
@@ -1,10 +1,53 @@
 import streamlit as st
 from config.styles import setup_page_config
 
+
+# --- КОНТРОЛЬ НАВИГАЦИИ ----------------------------------------
+
+def next_step():
+    """Обработка кнопки Next"""
+    if (st.session_state.step == 1 and len(st.session_state.data["image_names"]) > 0):
+        st.session_state.step += 1
+    else:
+        return
+
+
+def restart():
+    """Обработка кнопки Restart"""
+    st.session_state.step = 1
+    st.session_state.clear() # полная очистка всех состояний
+    init_session_state() # повторная инициализация
+
+
+
 def analysis():
+    """Точка входа в приложение"""
     setup_page_config("Analysis", "📈")
+    init_session_state() # инициализация переменных
+
     st.logo("logo.png", size = "large", icon_image=None)
-    st.info("ℹ️ Please upload data using the sidebar")
+    
+    # Боковая панель
+    if st.session_state.sidebar_state == "expanded":
+        # контент для каждого шага
+        with st.sidebar: 
+            if st.session_state.step == 1:
+                render_upload_sidebar()
+            elif st.session_state.step == 2:
+                render_result_sidebar()
+            
+            # Общие кнопки навигации ("Next" и "Restart")
+            col1, col2, col3 = st.columns([1, 1, 1])
+            with col1:
+                st.button("Restart", on_click=restart, disabled=st.session_state.step == 1)
+            with col2:
+                st.button("Next", on_click=next_step, disabled=((st.session_state.step == 1 and len(st.session_state.data["image_names"]) == 0) or (st.session_state.step == 2)))
+
+    # Основное окно
+    if st.session_state.step == 1:
+        render_upload_page()
+    elif st.session_state.step == 2:
+        render_result_page()
 
 
 # --- ВХОД ---
